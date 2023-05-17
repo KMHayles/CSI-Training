303d79610e6bc06fde40f703ac485258ab0820fd
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-05-17 03:33:52 -0500

    first push

---
+++ src/test/java/com/csi/training/CsiTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csi.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsiTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
