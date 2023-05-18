f4c8ed3717fceaadad399420503718e6cef9e276
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-05-18 03:52:33 -0500

    admin links added

--- src/main/java/com/csi/training/controllers/AdminDirectiveController.java
+++ src/main/java/com/csi/training/controllers/AdminDirectiveController.java
@@ -1,13 +1,15 @@
 package com.csi.training.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminDirectiveController {
 
-        @GetMapping("/AD")
-        public String returnAdminPage() {
-            return "users/admin";
-        }
+    @GetMapping("/AD")
+    public String returnAdminPage() {
+        return "users/admin";
+    }
+
 }
--- src/main/resources/static/js/AD.js
+++ src/main/resources/static/js/AD.js
@@ -1,4 +1,24 @@
-function showPDF() {
-    let pdfContainer = document.querySelector('.pdf-container');
-    pdfContainer.style.display = 'block';
+// function showPDF() {
+//     let pdfContainer = document.querySelector('.pdf-container');
+//     pdfContainer.style.display = 'block';
+// }
+
+// function togglePDF() {
+//     let pdfContainer = document.querySelector('.pdf-container');
+//
+//     if (pdfContainer.style.display === 'block') {
+//         pdfContainer.style.display = 'none';
+//     } else {
+//         pdfContainer.style.display = 'block';
+//     }
+// }
+
+function togglePDF(containerId) {
+    let pdfContainer = document.getElementById(`pdf-container-${containerId}`);
+
+    if (pdfContainer.style.display === 'block') {
+        pdfContainer.style.display = 'none';
+    } else {
+        pdfContainer.style.display = 'block';
+    }
 }
